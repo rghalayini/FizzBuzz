7691a9a4aa1a62547a5b81523642f45f9f1c4622
Remy Ghalayini <remyghalayini@gmail.com>
2020-11-02 20:36:33 +0100

    added c# solution

---
+++ FizzBuzz_Csharp/FizzBuzz_Csharp/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz_Csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insert how many integers you want in your FizzBuzz program:");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            string output = "";
+
+            for (int i = 1; i < n+1; i++)
+            {
+                if (i % 3 == 0 && i% 4 == 0)
+                    { 
+                        output += "FizzBuzz "; 
+                    }
+                else if (i % 3 == 0)
+                    { 
+                        output += "Fizz "; 
+                    }
+                else if (i % 4 == 0)
+                    { 
+                        output += "Buzz "; 
+                    }
+                else
+                    {
+                        output += i+" ";  
+                    }               
+            }
+            Console.WriteLine();
+            Console.WriteLine("Your FizzBuzz is:");
+            Console.WriteLine(output);
+            Console.ReadLine();
+        }
+    }  
+}
+
